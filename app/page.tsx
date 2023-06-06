10f354860f9b94585b913d3f9ef6de9995d3cdb8
Benny Nottonson <bennynottonson@gmail.com>
2023-06-05 22:15:29 -0700

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -17,18 +17,18 @@ export default function Home() {
 					<YoutubeVideo videoId="BVCNGxNCWzI" />
 				</section>
 				<Carousel />
-				<section className="bg-dark w-full h-40">
+				<section className="bg-dark w-full h-auto">
 					<h1 className="text-white text-center text-2xl pt-4 pb-4">
 						Support the dance program!
 					</h1>
-					<div className="mb-20 grid text-center lg:mb-0 lg:grid-cols-2 lg:text-center h-auto">
+					<div className="mb-4 grid text-center lg:mb-0 lg:grid-cols-2 lg:text-center h-auto">
 						<a
 							href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
 							className="group rounded-lg border border-transparent px-16 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
 							target="_blank"
 							rel="noopener noreferrer"
 						>
-							<h2 className={"mb-3 text-2xl font-semibold text-green-400"}>
+							<h2 className={"mb-3 text-2xl font-semibold text-green-400 group-hover:scale-110 duration-300"}>
 								Venmo{" "}
 								<span className="inline-block translate-y-1.5 transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
 									<CircleDollarSign size={24} />
@@ -41,9 +41,9 @@ export default function Home() {
 							target="_blank"
 							rel="noopener noreferrer"
 						>
-							<h2 className={"mb-3 text-2xl font-semibold text-green-400"}>
+							<h2 className={"mb-3 text-2xl font-semibold text-green-400 group-hover:scale-110 duration-300"}>
 								Paypal{" "}
-								<span className="inline-block translate-y-1.5 transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+								<span className="inline-block translate-y-1.5 transition-transform group-hover:translate-x-1 motion-reduce:transform-none duration-300">
 									<CircleDollarSign size={24} />
 								</span>
 							</h2>
