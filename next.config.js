601d6033725febb3ef4946b9bfb60bcba0587fc9
Benny Nottonson <bennynottonson@gmail.com>
2023-06-07 10:54:44 -0700

    pdfRefresher

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-module.exports = nextConfig;
+const fs = require('fs');
+const path = require('path');
+
+const refreshFiles = () => {
+    const handoutPath = path.join(__dirname, 'public', 'handouts');
+    const handoutFiles = fs.readdirSync(handoutPath);
+    fs.writeFileSync(path.join(__dirname, 'public/handout-pdfs.txt'), handoutFiles.join('\n'));
+}   
+
+
+module.exports = async (phase, { defaultConfig }) => {
+    refreshFiles();
+    return nextConfig
+}
