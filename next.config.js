94729b86842d344b764691b6a268bfae78353338
Benny Nottonson <bennynottonson@gmail.com>
2023-06-07 10:55:28 -0700

    fmt

--- next.config.js
+++ next.config.js
@@ -1,17 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 const refreshFiles = () => {
-    const handoutPath = path.join(__dirname, 'public', 'handouts');
-    const handoutFiles = fs.readdirSync(handoutPath);
-    fs.writeFileSync(path.join(__dirname, 'public/handout-pdfs.txt'), handoutFiles.join('\n'));
-}   
-
+	const handoutPath = path.join(__dirname, "public", "handouts");
+	const handoutFiles = fs.readdirSync(handoutPath);
+	fs.writeFileSync(
+		path.join(__dirname, "public/handout-pdfs.txt"),
+		handoutFiles.join("\n"),
+	);
+};
 
 module.exports = async (phase, { defaultConfig }) => {
-    refreshFiles();
-    return nextConfig
-}
+	refreshFiles();
+	return nextConfig;
+};
